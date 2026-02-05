3d76e213a32ad0e594f76a064b293ebb61755db7
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2026-02-05 20:15:34 -0300

    Use context API

--- ricklm/models/capabilities.py
+++ ricklm/models/capabilities.py
@@ -1,13 +1,21 @@
+from transformers import pipeline
+
 from ricklm.models._base import Model
 
 def normalize(response: str) -> str:
     return response.strip().replace("\\n", "\n").replace("\\t", "\t")
 
 class GeneratesText(Model):
+    _pipe: pipeline | None = None
+
+    def __enter__(self):
+        self._pipe = pipeline("text-generation")
+        return self
+    
+    def __exit__(self, exc_type, exc_value, traceback):
+        self._pipe.cache_clear()
 
     def text(self, prompt: str) -> str:
-        pipe = self.pipeline("text-generation")
         messages = [{"role": "user", "content": prompt},]
-        response = pipe(messages, max_new_tokens=512, do_sample=True, temperature=0.7)
-        self.pipeline.cache_clear()
+        response = self._pipe(messages, max_new_tokens=512, do_sample=True, temperature=0.7)
         return normalize(response[0]["generated_text"][-1]["content"])
