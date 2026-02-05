aa42785f01174b30e6f1b351bab8d1547c2b12e6
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2026-02-05 15:19:50 -0300

    Fix response from poem generation

--- ricklm/models/capabilities.py
+++ ricklm/models/capabilities.py
@@ -5,4 +5,5 @@ class GeneratesText(Model):
     def text(self, prompt: str) -> str:
         pipe = self.pipeline("text-generation")
         messages = [{"role": "user", "content": prompt},]
-        return pipe(messages, max_new_tokens=512, do_sample=True, temperature=0.7)
+        response = pipe(messages, max_new_tokens=512, do_sample=True, temperature=0.7)
+        return response[0]["generated_text"][-1]["content"]
