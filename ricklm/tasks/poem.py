82212847ad91bb641966c1f9fd0447baacb2ffae
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2026-02-05 15:41:25 -0300

    Add debug methods

--- ricklm/tasks/poem.py
+++ ricklm/tasks/poem.py
@@ -7,8 +7,22 @@ class Poem:
     model: GeneratesText = attrs.field(alias="by")
     style: str = attrs.field()
     title: str = attrs.field(default="", alias="titled")
+
+    @property
+    def prompt(self) -> str:
+        title_part = f' intitulado "{self.title}"' if self.title else ""
+        return f"Escreva um poema no estilo de {self.style}{title_part}."
+    
+    def full(self) -> str:
+        return "\n".join([
+            "Model",
+            str(self.model),
+            "Prompt:", 
+            self.prompt,
+            "",
+            "Poema:",
+            self.model.text(self.prompt)
+        ])
     
     def __str__(self) -> str:
-        title_part = f' intitulado "{self.title}"' if self.title else ""
-        prompt = f"Escreva um poema no estilo de {self.style}{title_part}."
-        return self.model.text(prompt)
+        return self.model.text(self.prompt)
