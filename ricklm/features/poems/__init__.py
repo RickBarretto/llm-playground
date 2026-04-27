b2f8de309a203661a2845592aa0a56088841af13
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2026-04-27 17:55:44 -0300

    Deprecate feature

--- ricklm/features/poems/__init__.py
+++ ricklm/features/poems/__init__.py
@@ -1,5 +1,4 @@
 from ricklm.features.poems.task import Poem
 from ricklm.features.poems import evaluate
-from ricklm.features.poems.examples import examples_at
 
-__all__ = ["Poem", "evaluate", "examples_at"]
+__all__ = ["Poem", "evaluate"]
--- ricklm/features/poems/examples.py
+++
@@ -1,7 +0,0 @@
-from pathlib import Path
-from typing import Iterable
-
-def examples_at(path: str) -> Iterable[str]:
-    samples = "Original"
-    for file in (Path(path) / samples).glob("[1-9].txt"):
-        yield file.read_text()
