8e5c7c04b005094d8633af0f6cc09bfa32542ff2
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2026-04-27 19:51:36 -0300

    Enable create mode for store_to

--- ricklm/features/store/__init__.py
+++ ricklm/features/store/__init__.py
@@ -3,5 +3,5 @@ from pathlib import Path
 __all__ = ["store_to"]
 
 def store_to(content: str, file: Path) -> None:
-    with file.open("w", encoding="utf-8") as f:
+    with file.open("w+", encoding="utf-8") as f:
         f.write(content)
